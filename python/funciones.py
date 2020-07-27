3ec4c45adcddbb9a3ca0c6260f91797d5f461303
KryptomSilver <abelromeror763@gmail.com>
2020-07-27 19:03:03 +0000

    Primeros archivos python

---
+++ python/funciones.py
@@ -0,0 +1,34 @@
+"""
+Nombre:funciones.py
+Objetivo:muestra la operacion de las funciones en python
+Autor:Abel Romero Ruiz
+Fecha: 27 julio de 2020
+"""
+def mensaje():
+	print("hola desde mensaje")
+def regresaMensaje():
+	return "saludo desde regresaMensaje"
+
+def printMensaje(msg):
+	print(msg)
+def suma(n1,n2):
+	return n1 + n2
+
+def main():
+	ciclo = 'S'
+	while ciclo == 'S' or ciclo == 's':
+		#invocamos funcion mensaje()
+		mensaje()
+		#invocamos funcion 
+		print(regresaMensaje())
+		#invocamos 
+		printMensaje("hola")
+		#leer datos
+		a = int(input("ingresa el primer numero entero: "))
+		b = int(input("ingresa el segundo numero: "))
+		print("La suma es: ",suma(a,b))
+		ciclo = input("Desea otra operacion (s/n)?: ")
+	else:
+		print("Fin de programa")
+if __name__ == "__main__":
+	main()
